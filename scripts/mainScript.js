0ab05cf05f8cb56986e9cfcaaf1efa0380c351f7
Dmitriy <dima030215@gmail.com>
2022-12-19 21:34:57 +0200

    initial commit

---
+++ scripts/mainScript.js
@@ -0,0 +1,69 @@
+// //_____Task-1_____//
+
+const arrayOne = ['a','b','c','d'];
+function functionOne () {
+   arrayOne.splice(0, 4,'e', 'f', 'g', 'h');
+   return arrayOne;
+}
+console.log(functionOne(arrayOne));
+
+
+
+
+// //_____Task-2_____//
+
+const firstArrayTwo = [1, 2, 3, 4, 5];
+const secondArrayTwo = [11, 12, 13, 14, 15];
+function functionTwo (elementATwo,elementBTwo) {
+   if (elementATwo.length !== elementBTwo.length) { // Сравнение длины массива
+      return false;
+   }
+   for (let i = 0; i < firstArrayTwo.length; i++) { //Цикл по всем элементам
+      if (elementATwo[i] !== elementBTwo[i]) {
+         return false;
+      }
+   }
+   return true;
+}
+console.log('Являются ли данные массивы идентичными ?',functionTwo(firstArrayTwo,secondArrayTwo));
+
+//_____Task-3_____//
+
+const arrayThree = ['Andrew','Dmitriy','Maksim','German','Dasha','Anastasia'];
+const letter = 'A';
+function functionFiltersItems (elementArray,elementLetter) {
+   return elementArray.filter((element) => element.startsWith(elementLetter));
+}
+console.log(functionFiltersItems(arrayThree,letter));
+
+// const arrayThree = ['Andrew','Dmitriy','Maksim','German','Dasha','Anastasia'];
+// function functionFiltersItems (elementArrayThree) {
+//    return elementArrayThree.filter((element) => element.startsWith('A'));
+// }
+// console.log(functionFiltersItems(arrayThree));
+
+// const arrayThree = ['Andrew','Dmitriy','Maksim','German','Dasha','Anastasia'];
+// const letter = 'A';
+// let array = [];
+// function funcSearch (elementArray,elementLetter) {
+//    for (let i = 0; i < elementArray.length; i++) {
+//       if (elementArray[i].startsWith(elementLetter)) {
+//          array.push(elementArray[i] + '');
+//       }
+//    }
+//    return array
+// }
+// console.log(funcSearch(arrayThree,letter));
+
+
+//_____Task-4_____//
+
+const arrayOfSentences = ['Hello world !','Hello, how are you ?','I can not play the games','I do not invite him'];
+const word = 'not';
+
+function functionSearchOfWord (elementArray,elementWord) {
+   return elementArray.filter((element) => element.includes(elementWord));
+}
+console.log(functionSearchOfWord(arrayOfSentences,word));
+
+ 
